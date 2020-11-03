7f91a61357e92fe120cb1b9096f830c4878383de
egbobright <egbobright@gmail.com>
2020-11-03 13:11:48 +0100

    my first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Creating A Stream Class
+### this illustration is from the book Node.js in Practice
+
+stream class will accept input as text, count word matches based on a regular expression
+
+And output the result is an event when the stream has finished been sent.
+
---
+++ index.js
@@ -0,0 +1,13 @@
+const CountStream = require('./countstream.js');
+
+const countStream = new CountStream('book');
+
+const http = require('http');
+
+http.get('http://www.manning.com', function(res){
+	res.pipe(countStream);
+});
+
+countStream.on('total', function(count){
+	console.log("Total matches: " + count);
+});
